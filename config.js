7032b1c8cd8aa44976b272abd6a63665b25d9376
aaditya-pro <121468299+aaditya-pro@users.noreply.github.com>
2026-08-26 18:29:46 +0545

    Update config.js

--- config.js
+++ config.js
@@ -2,3 +2,14 @@ const SUPABASE_URL = "https://apfqxlriilkwzynozsvv.supabase.co";
 const SUPABASE_KEY = "sb_publishable_AcwBiZcSMHrkfjV31JJK6A_r45OvAe4";
 
 const SUPABASE_BUCKET = "addy-files";
+
+if (!window.supabase) {
+    console.error("Supabase library not loaded.");
+} else {
+    window.supabaseClient = window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_KEY
+    );
+
+    console.log("Supabase connected successfully.");
+}
