03c0d5b91da1893e318ef9ed44ddcfbb7b098a05
aaditya-pro <121468299+aaditya-pro@users.noreply.github.com>
2026-08-26 18:56:45 +0545

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,20 @@
 const SUPABASE_URL = "https://apfqxlriilkwzynozsvv.supabase.co";
-const SUPABASE_KEY = "sb_publishable_AcwBiZcSMHrkfjV31JJK6A_r45OvAe4";
+
+const SUPABASE_KEY =
+    "sb_publishable_AcwBiZcSMHrkfjV31JJK6A_r45OvAe4";
+
 const SUPABASE_BUCKET = "addy-files";
+
+
+if (!window.supabase) {
+    throw new Error(
+        "Supabase library has not loaded. Check the Supabase CDN script."
+    );
+}
+
+
+const supabaseClient =
+    window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_KEY
+    );
